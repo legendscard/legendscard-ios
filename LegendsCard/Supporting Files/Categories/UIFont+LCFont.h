0229740bc177c46d1f7c85942fb276b38847c13a
Josh Sklar <jrmsklar@gmail.com>
2014-08-19 23:00:58 -0400

    create UIFont+LCFont

---
+++ LegendsCard/Supporting Files/Categories/UIFont+LCFont.h
@@ -0,0 +1,13 @@
+//
+//  UIFont+LCFont.h
+//  LegendsCard
+//
+//  Created by Josh Sklar on 8/19/14.
+//  Copyright (c) 2014 LegendsCard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFont (LCFont)
+
+@end
---
+++ LegendsCard/Supporting Files/Categories/UIFont+LCFont.m
@@ -0,0 +1,13 @@
+//
+//  UIFont+LCFont.m
+//  LegendsCard
+//
+//  Created by Josh Sklar on 8/19/14.
+//  Copyright (c) 2014 LegendsCard. All rights reserved.
+//
+
+#import "UIFont+LCFont.h"
+
+@implementation UIFont (LCFont)
+
+@end
