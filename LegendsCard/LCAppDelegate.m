acc76e860ce8cdcebcb8031d09c2e8f4d462ebe1
Josh Sklar <jrmsklar@gmail.com>
2014-08-19 23:08:57 -0400

    set tintColor to white
    This makes the back arrow on navigation bars white.

--- LegendsCard/LCAppDelegate.m
+++ LegendsCard/LCAppDelegate.m
@@ -40,6 +40,9 @@
     }
     
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    self.window.tintColor = [UIColor whiteColor];
+    
     // Override point for customization after application launch.
     self.viewController = [[LCHomeViewController alloc] init];
     self.window.rootViewController = self.viewController;
